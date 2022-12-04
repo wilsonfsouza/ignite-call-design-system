9f28162ef1c9c8c239315df5958bbbe9466b1b81
Wilson Franca <wilson.franca.92@gmail.com>
2022-12-03 19:04:22 -0800

    feat(setup): added initial config for stiches to use theme tokens

---
+++ packages/react/src/styles/index.ts
@@ -0,0 +1,37 @@
+import { createStitches, defaultThemeMap } from '@stitches/react'
+
+import {
+  colors,
+  fonts,
+  fontSizes,
+  fontWeights,
+  lineHeights,
+  radii,
+  space,
+} from '@ignite-ui/tokens'
+
+export const {
+  config,
+  createTheme,
+  css,
+  getCssText,
+  globalCss,
+  keyframes,
+  styled,
+  theme,
+} = createStitches({
+  themeMap: {
+    ...defaultThemeMap,
+    height: 'space',
+    width: 'space',
+  },
+  theme: {
+    colors,
+    fonts,
+    fontSizes,
+    fontWeights,
+    lineHeights,
+    radii,
+    space,
+  },
+})
