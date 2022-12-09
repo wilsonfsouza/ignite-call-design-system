ce1ed85852f2ee440b9732063aa76ece8d16c045
Wilson Franca <wilson.franca.92@gmail.com>
2022-12-08 19:52:20 -0800

    feat(docs): created ColorsGrid component to populate design sytem colors docs page

---
+++ packages/docs/src/components/ColorsGrid.tsx
@@ -0,0 +1,33 @@
+import { colors } from '@ignite-ui/tokens'
+import { getContrast } from 'polished'
+
+function getColorByContrastRatio(color: string) {
+  const DEFAULT_TEXT_COLOR = '#fff'
+  const DARK_TEXT_COLOR = '#111'
+
+  const contrastRatio = getContrast(color, DEFAULT_TEXT_COLOR)
+
+  const hasPoorContrastRatio = contrastRatio < 3.5
+
+  return hasPoorContrastRatio ? DARK_TEXT_COLOR : DEFAULT_TEXT_COLOR
+}
+
+export function ColorsGrid() {
+  return Object.entries(colors).map(([key, color]) => {
+    return (
+      <div key={key} style={{ backgroundColor: color, padding: '2rem' }}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            fontFamily: 'monospace',
+            color: getColorByContrastRatio(color),
+          }}
+        >
+          <strong>${key}</strong>
+          <span>{color}</span>
+        </div>
+      </div>
+    )
+  })
+}
