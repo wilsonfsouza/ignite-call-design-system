94c2946f2bf3c03ae5481a4077d454f21b10d365
Wilson Franca <wilson.franca.92@gmail.com>
2023-01-02 11:06:11 -0800

    feat(react/docs): created text component with its own stories file

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates consequuntur quas doloribus qui in est, saepe optio ratione neque ducimus omnis accusamus molestiae itaque aliquid odio, magnam, tenetur id tempore?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    as: 'strong',
+    children: 'Strong Text',
+  },
+}
--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,2 +1,3 @@
 export * from './components/Box'
 export * from './components/Button'
+export * from './components/Text'
