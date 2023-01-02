51c88aa756ffd0b081ef3e8a786cf573b7fd2301
Wilson Franca <wilson.franca.92@gmail.com>
2023-01-02 11:07:49 -0800

    refactor(docs): replaced styled span within Box stories to use the Text component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, BoxProps } from '@ignite-ui/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span style={{ color: '#fff' }}>Testing Box component</span>
-      </>
-    ),
+    children: <Text>Testing Box component</Text>,
   },
 } as Meta<BoxProps>
 
