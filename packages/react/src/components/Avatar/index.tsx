3fc447fa41732f0eddeaf9b098b4bdcacf29b965
Wilson Franca <wilson.franca.92@gmail.com>
2023-01-23 06:09:20 -0800

    feat(react/docs): created avatar component

---
+++ packages/react/src/components/Avatar/index.tsx
@@ -0,0 +1,17 @@
+import { ComponentProps } from 'react'
+import { AvatarContainer, AvatarFallback, AvatarImage } from './styles'
+import { User } from 'phosphor-react'
+
+export interface AvatarProps extends ComponentProps<typeof AvatarImage> {}
+
+export function Avatar(props: AvatarProps) {
+  return (
+    <AvatarContainer>
+      <AvatarImage {...props} />
+
+      <AvatarFallback delayMs={600}>
+        <User />
+      </AvatarFallback>
+    </AvatarContainer>
+  )
+}
