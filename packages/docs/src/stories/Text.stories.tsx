87130f50280cea17ab047d5ed4bae2a77ef6983e
Wilson Franca <wilson.franca.92@gmail.com>
2023-02-27 05:44:21 -0800

    feat(docs): added size control to Text component

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates consequuntur quas doloribus qui in est, saepe optio ratione neque ducimus omnis accusamus molestiae itaque aliquid odio, magnam, tenetur id tempore?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
