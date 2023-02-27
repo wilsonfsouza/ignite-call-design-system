eb52ab03a11b148f8b131bf23e8883207278d09b
Wilson Franca <wilson.franca.92@gmail.com>
2023-02-27 05:47:06 -0800

    feat(docs): added size control to Heading component

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -15,12 +15,21 @@ export const CustomTag: StoryObj<HeadingProps> = {
   args: {
     as: 'h1',
     children: 'H1 Heading',
+    size: 'md',
   },
   parameters: {
     docs: {
       description: {
         story:
           'By default, the `<Heading />` component will always be a `h2`; however, by using the `as` property, we can change the HTML tag to other headings. <br><br> For example, we can assign property `as` to a `h1`.',
+      },
+    },
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'inline-radio',
       },
     },
   },
