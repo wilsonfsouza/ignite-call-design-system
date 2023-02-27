4a212eddb546507181afd79e3af0c7b4f5864cbc
Wilson Franca <wilson.franca.92@gmail.com>
2023-02-27 05:49:09 -0800

    feat(docs): disabled appearance of box children in controll

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -7,6 +7,13 @@ export default {
   args: {
     children: <Text>Testing Box component</Text>,
   },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
 } as Meta<BoxProps>
 
 export const Primary: StoryObj<BoxProps> = {}
