b7354ab3107ad6f77572680247cc9651cad9a7ed
Wilson Franca <wilson.franca.92@gmail.com>
2023-02-27 05:50:31 -0800

    feat(docs): changed src control type to text

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/wilsonfsouza.png',
     alt: 'Wilson Franca',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
