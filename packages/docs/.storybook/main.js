59cf446ca30e28b2dd21070b24b7d21a66760bb7
Wilson Franca <wilson.franca.92@gmail.com>
2023-03-02 06:06:15 -0800

    fix(docs): added subpath to storybook on producetion

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -16,5 +16,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-call-design-system/'
+    }
+
+    return config
   }
 }
